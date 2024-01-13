da5657a360d2339870e6f15b5d3b7f33e0a2abf2
abhip2565 paul <paul.apaul.abhishek.AP@gmail.com>
2024-01-13 05:31:35 +0000

    Refactor.Assert

--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -1,9 +1,5 @@
 package com.thoughtworks.rectangle;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,7 +15,7 @@ public class RectangleTest {
 
     @Test
     public void shouldReturnAreaOfTwentyWhenLengthIsFiveAndBreadthIsFour() {
-        assertEquals( 20, rectangle1.area(),"Area should be calculated correctly");
+        assertThat(rectangle1.area(), is(equalTo(20)) );
     }
 
    
